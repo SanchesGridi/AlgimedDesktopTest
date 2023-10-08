ab65d821ebe5f90e78dc42458b4865b88449f8f6
Alexander Gridin <sanchesgridi@gmail.com>
2023-10-08 17:01:44 +0300

    conver [MainWindow] class to file-scoped namespace

--- AlgimedDesktopTest.WpfImplementation/Views/MainWindow.xaml.cs
+++ AlgimedDesktopTest.WpfImplementation/Views/MainWindow.xaml.cs
@@ -1,12 +1,11 @@
 ﻿using System.Windows;
 
-namespace AlgimedDesktopTest.WpfImplementation.Views
+namespace AlgimedDesktopTest.WpfImplementation.Views;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
