2f16a68c0778a69220fd4183c0b21a606b778bf4
Alexander Gridin <sanchesgridi@gmail.com>
2023-10-08 17:55:22 +0300

    initialize database project

---
+++ AlgimedDesktopTest.Database/Contexts/AppContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AlgimedDesktopTest.Database.Contexts;
+
+public class AppContext : DbContext
+{
+    public AppContext(DbContextOptions<AppContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
---
+++ AlgimedDesktopTest.Database/Entities/Base/IEntityBase.cs
@@ -0,0 +1,6 @@
+﻿namespace AlgimedDesktopTest.Database.Entities.Base;
+
+public interface IEntityBase<TAny>
+{
+    public TAny Id { get; set; }
+}
